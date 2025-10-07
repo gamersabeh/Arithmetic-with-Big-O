9c6596e5759d582d049904ecb51c81bf13e6975d
Mohamad Sabeh <mohamadsabeh1234@gmail.com>
2025-10-07 13:51:38 +0300

    Add linear search function

--- Code.py
+++ Code.py
@@ -14,3 +14,12 @@ def find_Max(nbs):
             max_val = i
     return max_val
 # Time Complexity: O(n)
+
+# Linear Search
+def linear_search(lst, target):
+    for i in lst:
+        if i == target:
+            return True
+    return False
+# Time Complexity: O(n)
+
